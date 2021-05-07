7adaa6bd8af40e92f7f01a0de48da820b12d1905
quarterdill <dylanx177@gmail.com>
2021-05-07 17:40:33 -0400

    Final commit

--- server.js
+++ server.js
@@ -4,36 +4,6 @@ const app = express()
 const mongoose = require('mongoose')
 const Recipe = require('./models/recipe')
 const {MongoClient} = require('mongodb');
-const url = 'mongodb://localhost/Recipes'
-const {ObjectId} = require('mongodb');
-const id = ObjectId("5ff0e136d7557de1509e2d36");
-
-// const password = encodeURIComponent("9LzwsS4rsuWZvMx1")
-
-/*mongoose.connect(url, { 
-    useNewUrlParser: true, useUnifiedTopology: true//, useCreateIndex: true
-})*/
-
-const query = {"recipe": "pizza"};
-//const client = new MongoClient(uri, {useNewUrlParser: true}, {useUnifiedTopology: true}, {connectTimeoutMS: 30000}, {keepAlive: 1});
-
-const dbName = 'DillRecipes'
-// let db
-
-// MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
-//   if (err) return console.log(err)
-
-//   // Storing a reference to the database so you can use it later
-//   db = client.db(dbName)
-//   console.log(`Connected MongoDB: ${uri}`)
-//   console.log(`Database: ${dbName}`)
-
-//   var cursor = db.orders.find({}, {"recipe": "Pizza"});
-//   cursor.each(function(err, doc) {
-//       console.log(doc);
-//   })
-// }) 
-
 
 //listDatabases of cluster function
 async function listDatabases(client){
@@ -68,34 +38,6 @@ async function listDatabases(client){
 // main().catch(console.error);
 
 
-async function findOneListingByName(nameOfListing) {
-    const uri = process.env.MONGO_URI;
-    
-    const client = new MongoClient(uri, { useUnifiedTopology: true });
-
-    try {
-        // Connect to the MongoDB cluster
-        await client.connect();
- 
-        result = await client.db("DillRecipes").collection("recipes")
-                        .findOne({ recipe: nameOfListing });
- 
-    } catch (e) {
-        console.error(e);
-    } finally {
-        await client.close();
-    }
-
-    if (result) {
-        console.log(`Found a listing in the collection with the name '${nameOfListing}':`);
-        console.log(result);
-    } else {
-        console.log(`No listings found with the name '${nameOfListing}'`);
-    }
-}
-
-
-
 const recipeNames = ["Pizza", "Brownies"];
 
 let recipeArray = new Array(recipeNames.length);
@@ -125,58 +67,10 @@ async function findRecipe(nameOfRecipe) {
     }
 }
 
-// async function makeRecipes() {
-//     for(i = 0; i < recipeNames.length; i++) {
-//         recipeArray[i] = await findRecipe(recipeNames[i]);
-//     }
-// }
-// console.log(recipeArray);
-
-//findOneListingByName("Brownies").catch(console.error);
-//findRecipe("Pizza").catch(console.error);
-
-
-for(i = 0; i< recipeNames.length; i++) {
-    let rec = recipeNames[i];
-    recipeArray[i] = findRecipe(rec);
-}
-
-function test() {
-Promise.all(recipeArray).then((values) => {
-    return values;
-  });
-}
-
-
-// findRecipe(rec).then(function(result) {
-//     recipeArray[i] = result;
-//     console.log(result)
-// })
-
-
-// client.connect(err => {
-//   const collection = client.db("DillRecipes").collection("recipes");
-//   console.log(collection);
-//   // perform actions on the collection object
-//   client.close();
-// });
-
-
-// client.connect(err => {
-//     //const collection = client.db("test").collection("devices");
-//     //console.log(collection)
-//     console.log("Success!")
-//     const recip = client.db("DillRecipes").collection("recipes").findOne({"_id": id}, {});
-//     console.log(recip);
-//   // perform actions on the collection object
-//     client.close();
-// });
-
-
-//var names = ["Pizza", "Brownies"];
 
 app.set('view engine', 'ejs')
 
+// set the files we can access, css and img
 app.use(express.static(__dirname + '/public/css'));
 app.use(express.static(__dirname + '/public/img'));
 
@@ -197,3 +91,39 @@ if (port == null || port == "") {
   port = 5000;
 }
 app.listen(port);
+
+
+// async function makeRecipes() {
+//     for(i = 0; i < recipeNames.length; i++) {
+//         recipeArray[i] = await findRecipe(recipeNames[i]);
+//     }
+// }
+// console.log(recipeArray);
+
+//findRecipe("Pizza").catch(console.error);
+
+// function to get all recipes
+// function test() {
+// Promise.all(recipeArray).then((values) => {
+//     return values;
+//   });
+// }
+
+
+// failed attempts to connect to mongodb database
+// client.connect(err => {
+//   const collection = client.db("DillRecipes").collection("recipes");
+//   console.log(collection);
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+// client.connect(err => {
+//     //const collection = client.db("test").collection("devices");
+//     //console.log(collection)
+//     console.log("Success!")
+//     const recip = client.db("DillRecipes").collection("recipes").findOne({"_id": id}, {});
+//     console.log(recip);
+//   // perform actions on the collection object
+//     client.close();
+// });
